8ed5af94d5de9c1e2447ddeb7feae6539ed35d7d
root <root@myofc.(none)>
2009-10-15 18:13:08 +0800

    增加例子

--- test_mysql.lua
+++ test_mysql.lua
@@ -20,6 +20,7 @@ print(db:set_charset("utf8")) -- there is no char '-'
 print(db:query("insert `table` (`hits`,`time`,`col1`,`col2`) values (10000, 33333, '天使', 'hehe')"))
 print(db:insert_id())
 print(db:escape_string('哈哈"'))
+print(db:real_escape_string([['哈哈`"'\n]]))
 os.exit();
 local rs = db:query("select * from `table`")
 
